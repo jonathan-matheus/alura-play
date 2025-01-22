20fd095e31716ab2eb9706a32174d3c26b891e72
jonathan-matheus <johhn.dev@gmail.com>
2025-01-22 13:27:39 -0300

    first commit

---
+++ app/novo-video.php
@@ -0,0 +1,27 @@
+<?php
+
+use Alura\Mvc\Entity\Video;
+use Alura\Mvc\Repository\VideoRepository;
+
+$dbPath = __DIR__ . '/banco.sqlite';
+$pdo = new PDO("sqlite:$dbPath");
+
+$url = filter_input(INPUT_POST, 'url', FILTER_VALIDATE_URL);
+if ($url === false) {
+    header('Location: /?sucesso=0');
+    exit();
+}
+
+$titulo = filter_input(INPUT_POST, 'titulo');
+if (is_null($titulo) || $titulo === false) {
+    header('Location: /?sucesso=0');
+    exit();
+}
+
+$repository = new VideoRepository($pdo);
+
+if ($repository->add(new Video($url, $titulo)) == false) {
+    header('Location: /?sucesso=0');
+} else {
+    header('Location: /?sucesso=1');
+}
